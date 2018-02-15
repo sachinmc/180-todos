9d05eb227fc4dcbdb0441f8c033e330f20e7a46a
Sachin Chandy <sachinmchandy@gmail.com>
2018-02-15 10:28:43 +0000

    N + 1 Query Optimization, Moved business logic from APP to the DB, Standardized the way list hashes are used in the APP, Changed the way we load data

--- database_persistence.rb
+++ database_persistence.rb
@@ -12,7 +12,7 @@ class DatabasePersistence
 
   def disconnect
     @db.close
-  end 
+  end
 
   def query(statement, *params)
     @logger.info "#{statement}: #{params}"
@@ -20,25 +20,36 @@ class DatabasePersistence
   end
 
   def find_list(id)
-    sql = "SELECT * FROM lists WHERE id =$1"
+    sql = <<~SQL
+          SELECT lists.*,
+          count(todos.id) AS todos_count,
+          count(NULLIF(todos.completed, true)) AS todos_remaining_count
+          FROM lists
+          LEFT JOIN todos ON lists.id = list_id
+          WHERE lists.id = $1
+          GROUP BY lists.id
+          ORDER BY lists.name;
+    SQL
+
     result = query(sql, id)
 
-    tuple = result.first
-
-    list_id = tuple["id"].to_i
-    todos = find_todos_for_lists(list_id)
-    { id: list_id, name: tuple["name"], todos: todos }
+    tuple_to_list_hash(result.first)
   end
 
   def all_lists
-    sql = "SELECT * FROM lists"
+    sql = <<~SQL
+          SELECT lists.*,
+          count(todos.id) AS todos_count,
+          count(NULLIF(todos.completed, true)) AS todos_remaining_count
+          FROM lists
+          LEFT JOIN todos ON lists.id = list_id
+          GROUP BY lists.id
+          ORDER BY lists.name;
+    SQL
     result = query(sql)
 
     result.map do |tuple|
-      list_id = tuple["id"].to_i
-      todos = find_todos_for_lists(list_id)
-
-      { id: list_id, name: tuple["name"], todos: todos }
+      tuple_to_list_hash(tuple)
     end
   end
 
@@ -77,8 +88,6 @@ class DatabasePersistence
     query(sql, list_id)
   end
 
-  private
-
   def find_todos_for_lists(list_id)
     todo_sql = "SELECT * FROM todos WHERE list_id = $1"
     todos_result = query(todo_sql, list_id)
@@ -88,5 +97,14 @@ class DatabasePersistence
         name: todo_tuple["name"],
         completed: todo_tuple["completed"] == 't' }
     end
+  end
+
+  private
+
+  def tuple_to_list_hash(tuple)
+    { id: tuple["id"].to_i,
+      name: tuple["name"],
+      todos_count: tuple["todos_count"].to_i,
+      todos_remaining_count: tuple["todos_remaining_count"].to_i }
   end
 end
